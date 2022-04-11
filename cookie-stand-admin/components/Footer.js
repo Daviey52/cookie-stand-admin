454c1b02b799a3b666dd67413a05a64022f21f25
Daviey52 <davidkariska@gmail.com>
2022-04-11 04:44:01 -0700

    overview

--- cookie-stand-admin/components/Footer.js
+++ cookie-stand-admin/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="p-4 bg-green-500 text-black-50 mt-10">
-      <h3>@2021</h3>
+      <h3>Locations World Wide</h3>
     </footer>
   )
 }
