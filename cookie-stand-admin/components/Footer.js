900c36a58b62f753919e29bf4195fa12e2ad9277
Daviey52 <davidkariska@gmail.com>
2022-04-15 08:37:35 -0700

    finished

--- cookie-stand-admin/components/Footer.js
+++ cookie-stand-admin/components/Footer.js
@@ -1,7 +1,7 @@
-export default function Footer() {
+export default function Footer(props) {
   return (
     <footer className="p-4 bg-green-500 text-black-50 mt-10">
-      <h3>Locations World Wide</h3>
+      <h3>{props.report} Locations World Wide</h3>
     </footer>
   )
 }
--- cookie-stand-admin/pages/index.tsx
+++ cookie-stand-admin/pages/index.tsx
@@ -1,39 +1,17 @@
 import type { NextPage } from 'next'
-
-import Image from 'next/image'
-import { useState } from 'react'
 import Header from '../components/Header'
-import Footer from '../components/Footer'
 import Head from '../components/Head'
 import Main from '../components/Main'
+
 const Home: NextPage = () => {
 
 
-  const [location, setLocation] = useState('None');
-  const [maximum, setmax] = useState(0);
-  const [minimum, setmin] = useState(0);
-  const [average, setavg] = useState(0);
-
-  function addDataHander(event) {
-    event.preventDefault();
-    setLocation(event.target.location.value);
-    setmin(event.target.min.value);
-    setmax(event.target.max.value);
-    setavg(event.target.avg.value);
-
-
-  }
   return (
     <div>
       <Head />
       <Header />
-      <Main addDataHander={addDataHander}
-        location={location}
-        minimum={minimum}
-        maximum={maximum}
-        average={average}
+      <Main
       />
-      <Footer />
     </div >
   )
 }
